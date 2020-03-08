e24ef1cd16d66b6b7cca3b99df3cc1026b80abb9
VioletMyth <jayson.tai@hotmail.com>
2020-03-09 01:02:43 +1300

    correct answer

--- Zero matrix 1.8.py
+++ Zero matrix 1.8.py
@@ -9,15 +9,26 @@ def zeroMatrix(matrix):
                 positions["y"].append(j)
             
             j += 1
+        j = 0
         i += 1
     i,j = 0,0
     while i < len(matrix):
-        if i in positions["x"]:
-            while j < len(matrix[i]):
+        while j < len(matrix[i]):
+            if i in positions["x"]:
                 matrix[i][j] = 0
-                j += 1
+            j += 1
+        j = 0
         i+=1
+    
+    row,column = 0,0
+    while column < len(matrix[row]):
+        while row < len(matrix):
+            if column in positions["y"]:
+                matrix[row][column] = 0
+            row += 1
+        row = 0
+        column += 1
 
     return matrix
 
-print(zeroMatrix([[0,1,2], [1,2,3], [1,2,3]]))
+print(zeroMatrix([[0,1,1], [1,2,1], [2,3,1], [2,0,1]]))
